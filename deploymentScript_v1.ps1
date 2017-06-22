18d86c06e33b8065291a7cdaf3428a371276ea58
tornizanQ <tor.n@qualisystems.com>
2017-06-22 13:59:48 +0300

    Update deploymentScript_v1.ps1

--- deploymentScript_v1.ps1
+++ deploymentScript_v1.ps1
@@ -1,10 +1,15 @@
 Write-Host "Start of config script"
+
+
 $version_name=Get-ChildItem Env:quali_version
+Write-Host $version_name
+
 #can be Server,Portal,ES
 $product_name=Get-ChildItem Env:quali_product
-$server_ip=Get-ChildItem Env:server
+Write-Host $product_name
 
-Write-Host $version_name $product_name $server_ip
+$server_ip=Get-ChildItem Env:server
+Write-Host $server_ip
 
 $qs_setup_path = '"' +"\\qsnas1\Shared\Tor\"+ $version_name +"\CloudShell\Data\QsSetup.exe"+'"'
 
@@ -15,6 +20,7 @@ $server_answer_file = '"'+"\\qsnas1\Shared\Tor\"+ $version_name +"\CloudShell\Ut
 $portal_answer_file = '"'+"\\qsnas1\Shared\Tor\"+ $version_name +"\CloudShell\Utilities\AnswerFiles\CloudShellPortalOnlyAnswersFile.xml"+ '"'
 $es_answer_file     = '"'+"\\qsnas1\Shared\Tor\"+ $version_name +"\CloudShell\Utilities\AnswerFiles\CloudShellESOnlyAnswersFile.xml"+ '"'
 $portal_customer_config = '"'+"\\qsnas1\Shared\Tor\"+ $version_name +"\CloudShell\Utilities\AnswerFiles\Portal\customer.config"+ '"'
+
 if ( $product_name -eq"server"){
 	$answer_file=$server_answer_file
 } 
@@ -22,20 +28,18 @@ if ( $product_name -eq"server"){
 if ( $product_name -eq "portal"){
 	$answer_file=$portal_answer_file
 	Get-Item $portal_customer_config | Replace-FileString
-
-  -Pattern 'localhost'
-
-  -Replacement $server_ip
+	-Pattern 'localhost'
+	-Replacement $server_ip
 }
 
 if ( $product_name -eq "es"){
 	$answer_file=$es_answer_file
 }
 
+
 #Silent install the requested file
 $args ="/unattended /answers:" + $answer_file
-
-
-Write-Host $args;
+Write-Host ----->args:
+Write-Host $args
 
 Start-Process -FilePath $qs_setup_path -ArgumentList  $args
