30b93c333a3a60a313dd6d73acd61d1142492f17
tornizanQ <tor.n@qualisystems.com>
2017-06-22 14:40:39 +0300

    Update deploymentScript_v1.ps1

--- deploymentScript_v1.ps1
+++ deploymentScript_v1.ps1
@@ -2,22 +2,25 @@ Write-Host "Start of config script"
 
 
 $version_name=Get-ChildItem Env:quali_version
+$version_name=$version_name.Value
 Write-Host $version_name
 
 #can be Server,Portal,ES
 $product_name=Get-ChildItem Env:quali_product
+$product_name=$product_name.Value
 Write-Host $product_name
 
 $server_ip=Get-ChildItem Env:server
+$server_ip=$server_ip.Value
 Write-Host $server_ip
 
-$qs_setup_path = '"' +"\\qsnas1\Shared\Tor\"+ $version_name +"\CloudShell\Data\QsSetup.exe"+'"'
+$qs_setup_path = '"' +"\\qsnas1\Shared\Tor\"+ $version_name.Value +"\CloudShell\Data\QsSetup.exe"+'"'
 
 Write-Host $qs_setup_path
 
 $answer_file=""
 $server_answer_file = '"'+"\\qsnas1\Shared\Tor\"+ $version_name +"\CloudShell\Utilities\AnswerFiles\CloudShellServerOnlyAnswersFile.xml"+ '"'
-$portal_answer_file = '"'+"\\qsnas1\Shared\Tor\"+ $version_name +"\CloudShell\Utilities\AnswerFiles\CloudShellPortalOnlyAnswersFile.xml"+ '"'
+$portal_answer_file = '"'+"\\qsnas1\Shared\Tor\"+ $version_name+"\CloudShell\Utilities\AnswerFiles\CloudShellPortalOnlyAnswersFile.xml"+ '"'
 $es_answer_file     = '"'+"\\qsnas1\Shared\Tor\"+ $version_name +"\CloudShell\Utilities\AnswerFiles\CloudShellESOnlyAnswersFile.xml"+ '"'
 $portal_customer_config = '"'+"\\qsnas1\Shared\Tor\"+ $version_name +"\CloudShell\Utilities\AnswerFiles\Portal\customer.config"+ '"'
 
