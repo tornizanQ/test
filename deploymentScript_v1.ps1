d301d1d60d38113f98e9b625470665fca79bf9b6
tornizanQ <tor.n@qualisystems.com>
2017-06-25 12:01:12 +0300

    Update deploymentScript_v1.ps1

--- deploymentScript_v1.ps1
+++ deploymentScript_v1.ps1
@@ -39,7 +39,7 @@ if ( $product_name -eq "es"){
 	$answer_file=$es_answer_file
 }
 
-$batContent =  """$qs_setup_path"" /silent /answers:""$answer_file"" >> c:\results.txt"
+$batContent =  ""$qs_setup_path" /silent /answers:""$answer_file"" >> c:\results.txt"
 Add-Content "C:\InstallSuite.bat" $batContent
 $psexec = "cmd.exe /c 'c:\InstallSuite.bat'"
 Invoke-Expression -Command:$psexec
